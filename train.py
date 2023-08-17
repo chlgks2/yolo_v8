57f056dd503f0b185d3bffaa662b2cb4a678e64e
chlgks2 <duddngud3@gmail.com>
2023-08-17 10:20:21 +0900

    #수정 넣음

--- train.py
+++ train.py
@@ -10,3 +10,5 @@ print(ultralytics.checks())
 if __name__ == '__main__':
     model = YOLO('yolov8n.pt')
     model.train(data='D:/myprj/yolometa/Fish-44/data.yaml',imgsz=640,batch=4,epochs=30,device=0)
+
+#수정
